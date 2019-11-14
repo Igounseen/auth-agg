99a0361e083ac19913afcdbfd09cff5aa2c0de8b
shiwenxiang <1030975178@qq.com>
2019-11-14 23:00:23 +0800

    postman 测试

---
+++ auth-service/auth-auth/src/main/java/com/swx/auth/auth/dto/PermissionDto.java
@@ -0,0 +1,16 @@
+package com.swx.auth.auth.dto;
+
+import lombok.Data;
+
+@Data
+public class PermissionDto {
+
+    private String id;
+
+    private String code;
+
+    private String description;
+
+    private String url;
+
+}
